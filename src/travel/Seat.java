cc9e985f05453b74bc43424612af5924451a86f1
Logan Earl <iearl1@eagles.ewu.edu>
2018-05-26 20:25:35 -0700

    updated travel package

--- src/travel/Seat.java
+++ src/travel/Seat.java
@@ -1,5 +1,36 @@
 package travel;
 
-public interface Seat {
-	public boolean isOccupied();
+public class Seat 
+{
+	private String name;
+	private boolean occupied;
+	
+	public Seat(String name) 
+	{
+		this.name = name;
+		this.occupied = false;
+	}
+	public Seat(int row, int col) {
+		this(convertName(row,col));
+	}
+	
+	private static String convertName(int row, int col) 
+	{
+		return (char)(col + 65) + "" + row;
+	}
+	
+	public void fill() 
+	{
+		this.occupied = true;
+	}
+	
+	public String getName() 
+	{
+		return this.name;
+	}
+	
+	public boolean isOccupied() 
+	{
+		return this.occupied;
+	}
 }
--- src/travel/Section.java
+++ src/travel/Section.java
@@ -1,14 +1,55 @@
 package travel;
 
-public abstract class Section {
+public abstract class Section 
+{
 	private Seat[][] seats;
 	private String name;
 	
-	public Section(String name) {
-		
+	protected Section(String name, int numRows, int numCols) 
+	{
+		seats = new Seat[numRows][];
+		for(int i = 0; i < numRows; i++) 
+		{
+			seats[i] = new Seat[numCols];
+			for(int j = 0; j < numCols; j++) 
+				seats[i][j] = new Seat(i,j);
+		}
+		this.name = name;
 	}
 	
-	public boolean hasAvailableSeat() {
+	protected boolean hasAvailableSeat() 
+	{
+		for(int i = 0; i < seats.length; i++) 
+			for(int j = 0; j < seats[i].length; j++)
+				if(!seats[i][j].isOccupied())
+					return true;
 		return false;
+	}
+	
+	protected boolean bookSeat() 
+	{
+		for(int i = 0; i < seats.length; i++) 
+			for(int j = 0; j < seats[i].length; j++)
+				if(!seats[i][j].isOccupied()) {
+					seats[i][j].fill();
+					return true;
+				}
+			
+		
+		return false;
+	}
+	protected boolean bookSeat(int row, int col) 
+	{
+		if(!seats[row][col].isOccupied()) 
+		{
+			seats[row][col].fill();
+			return true;
+		}
+		return false;
+			
+	}
+	protected String getName() 
+	{
+		return this.name;
 	}
 }
