13136e4ba82db8d1384f19b7f24f4811313ceb05
KawaiiJess <KawaiiJessJ@Gmail.com>
2018-05-26 18:28:47 -0700

    Making sure git is working.

---
+++ src/SystemManager.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SystemManager
+{
+    private HashMap<String, Airport> airports;
+    private HashMap<String, Airline> airlines;
+
+    SystemManager()
+    {
+
+    }
+
+    public void createAirport(String name)
+    {
+
+    }
+
+    public void createAirline(String name)
+    {
+
+    }
+
+    public void createFlight(String aname, String orig, String dest, int year, int month, int day, String id)
+    {
+
+    }
+
+    public void createSection(String air, String flID, int rows, int cols, SeatClass s)
+    {
+
+    }
+}
